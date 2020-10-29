eb0ec0e897ed249ccf92ca72236a0792439eedd2
Ruben Vermeersch <ruben@rocketeer.be>
2020-10-29 21:24:18 +0100

    Add benchmark

---
+++ fixtures/benchmarks_test.go
@@ -0,0 +1,42 @@
+package fixtures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+
+func BenchmarkParseBasicGenerated(b *testing.B) {
+	assert := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		_, err := ParseBasic(inBasic)
+		assert.NoError(err)
+	}
+}
+
+func BenchmarkParseBasicYAML(b *testing.B) {
+	assert := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		obj := &Basic{}
+		err := yaml.Unmarshal([]byte(inBasic), obj)
+		assert.NoError(err)
+	}
+}
+
+func BenchmarkParseInlineMapGenerated(b *testing.B) {
+	assert := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		_, err := ParseInlineMap(inInlineMap)
+		assert.NoError(err)
+	}
+}
+
+func BenchmarkParseInlineMapYAML(b *testing.B) {
+	assert := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		obj := &InlineMap{}
+		err := yaml.Unmarshal([]byte(inInlineMap), obj)
+		assert.NoError(err)
+	}
+}
